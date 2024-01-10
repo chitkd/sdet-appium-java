78f146dd88db99ede4c34d33e9a704377f131720
KimChi2893 <dtkchi2893@gmail.com>
2024-01-10 21:41:17 +0700

    init commit

---
+++ src/lessons/lesson_1/Hello.java
@@ -0,0 +1,11 @@
+package lessons.lesson_1;
+
+public class Hello {
+    public static void main(String[] args) {
+        // naming convention: camelCase, snake_case, PascalCase
+        System.out.println(Short.MAX_VALUE);
+
+        // java: incompatible types: possible lossy conversion from int to byte
+        // byte myByte = 32768;
+    }
+}
